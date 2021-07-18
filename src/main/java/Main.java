e6143dad0a2585ce3ed2144e53dc0397a3ac7c8e
DESKTOP-SKN51UP\ehwns <ehwnsdud1004@naver.com>
2021-07-18 18:17:22 +0900

    Edit Main For Simple

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void manual()
-    {
-        System.out.println("메뉴 확인 : 메뉴");
-        System.out.println("제조법 확인 : [메뉴명]");
-        System.out.println("예시 : 바닐라라떼(ICE) --> (ICE/HOT)여부까지 정확하게 입력해 주세요!");
-        System.out.println("종료 : 수고하세요");
-    }
-
-    // 메뉴 입력
-    public static void init(Menu menu)
-    {
-        menu.typeList.add("음료");
-
-        String[] strings = {"물","에스프레소","얼음"};
-        menu.setMenuItem("아메리카노(ICE)","커피",strings);
-        strings = new String[]{"물", "에스프레소"};
-        menu.setMenuItem("아메리카노(HOT)","커피",strings);
-        strings = new String[]{"우유", "에스프레소","얼음"};
-        menu.setMenuItem("카페라떼(ICE)","커피",strings);
-        strings = new String[]{"우유", "에스프레소"};
-        menu.setMenuItem("카페라떼(HOT)","커피",strings);
-        strings = new String[]{"우유", "에스프레소","바닐라 시럽","얼음"};
-        menu.setMenuItem("바닐라라떼(ICE)","커피",strings);
-        strings = new String[]{"우유", "에스프레소","바닐라 시럽"};
-        menu.setMenuItem("바닐라라떼(HOT)","커피",strings);
-
-        strings = new String[]{"우유", "딸기"};
-        menu.setMenuItem("딸기 스무디","스무디",strings);
-        strings = new String[]{"우유", "요거트"};
-        menu.setMenuItem("요거트 스무디","스무디",strings);
-
-        strings = new String[]{"물", "아이스티 분말","얼음"};
-        menu.setMenuItem("아이스티","주스",strings);
-    }
-
     public static void main(String[] args) {
         String command = "";
         Scanner scanner = new Scanner(System.in);
 
+        CoffeeShop coffeeShop = new CoffeeShop();
         Menu menu = new Menu();
         Barista barista = new Barista();
-        init(menu);
-        manual();
+
+        coffeeShop.init(menu);
+        coffeeShop.showManual();
 
         while(true)
         {
-            System.out.println();
             System.out.println("=>무엇을 도와드릴까요?");
             command = scanner.nextLine();
             if(command.equals("메뉴"))
