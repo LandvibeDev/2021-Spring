f7e15dbad708a5816579874a540b23f33d7d8247
somyeong <thaud8487@naver.com>
2021-07-22 14:56:56 +0900

    week1 과제 올립니당

---
+++ src/cafe/java/MenuItem.java
@@ -0,0 +1,12 @@
+class MenuItem {
+    int menu_number;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
